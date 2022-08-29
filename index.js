cf83bf2c9f86d95e95c0287ba0d411f5c42f026e
pragatib <bhandariprag7@gmail.com>
2022-08-29 15:13:26 +0300

    Used PostgreSQL with node.js and express and connect into database using Node.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./db/dbconfig')
+const app = express()
+app.use(bodyParser.json())
+
+const port = 3000
+
+// Get all movies
+app.get('/api/movies', (req, res) => {
+  db.query('SELECT * FROM movies', (err, result) => {
+    if (err) console.error(err)
+    else res.json(result.rows)
+  })
+})
+
+// Get movie by id
+app.get('/api/movies/:id', (req, res) => {
+  const query = {
+    text: 'SELECT * FROM movies WHERE id = $1',
+    values: [req.params.id],
+  }
+
+  db.query(query, (err, result) => {
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    } else {
+      if (result.rows.length > 0) res.json(result.rows)
+      else res.status(404).end()
+    }
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`)
+})
