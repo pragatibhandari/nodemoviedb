bfdec3d5dbaf67ef5bdc232f2ae426d97e62d39d
pragatib <bhandariprag7@gmail.com>
2022-08-29 16:27:51 +0300

    fetched data from the database using Node.js

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const db = require('./db/dbconfig')
+const query = require('./db/movies')
+
 const app = express()
 app.use(bodyParser.json())
 
 const port = 3000
 
-// Get all movies
-app.get('/api/movies', (req, res) => {
-  db.query('SELECT * FROM movies', (err, result) => {
-    if (err) console.error(err)
-    else res.json(result.rows)
-  })
-})
-
-// Get movie by id
-app.get('/api/movies/:id', (req, res) => {
-  const query = {
-    text: 'SELECT * FROM movies WHERE id = $1',
-    values: [req.params.id],
-  }
-
-  db.query(query, (err, result) => {
-    if (err) {
-      return console.error('Error executing query', err.stack)
-    } else {
-      if (result.rows.length > 0) res.json(result.rows)
-      else res.status(404).end()
-    }
-  })
-})
+app.get('/api/movies', query.getAllMovies)
+app.get('/api/movies/:id', query.getMovieById)
+app.post('/api/movies', query.addMovie)
+app.delete('/api/movies/:id', query.deleteMovie)
 
 app.listen(port, () => {
+  {
+  }
   console.log(`Server is running on port ${port}.`)
 })
